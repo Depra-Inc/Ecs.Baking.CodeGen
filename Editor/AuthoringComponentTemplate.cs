78fa3ab1677bdef829934a856826b56a28d73ee3
godzzz <g0dzZz1lla@yandex.ru>
2023-11-07 00:08:03 +0400

    [v0.0.6]: AuthoringComponentGenerator updated

---
+++ Editor/AuthoringComponentTemplate.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Reflection;
+using Depra.Ecs.Hybrid.Components;
+using UnityEngine;
+
+namespace Depra.Ecs.Hybrid.CodeGen.Editor
+{
+	internal static class AuthoringComponentTemplate
+	{
+		public static string GenerateFactory(int componentFieldsCount) => componentFieldsCount == 0
+			? @$"
+	{nameof(IBaker)} {nameof(IAuthoring)}.{nameof(IAuthoring.CreateBaker)}() => new Baker();
+"
+			: @$"
+	{nameof(IBaker)} {nameof(IAuthoring)}.{nameof(IAuthoring.CreateBaker)}() => new Baker(this);
+";
+
+		public static string GenerateFields(FieldInfo[] componentFields)
+		{
+			var authoringFields = string.Empty;
+			if (componentFields.Length <= 0)
+			{
+				return authoringFields;
+			}
+
+			authoringFields += "\n";
+			return componentFields.Aggregate(authoringFields, (current, field) => current +
+				$"\t[{nameof(SerializeField)}] private {field.FieldType.FullName} {field.Name.ToCamelCaseWithUnderscore()};\n");
+		}
+	}
+}
---
+++ Editor/BakerTemplate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Depra.Ecs.Components;
+using Depra.Ecs.Hybrid.Components;
+using Depra.Ecs.Worlds;
+
+namespace Depra.Ecs.Hybrid.CodeGen.Editor
+{
+	internal static class BakerTemplate
+	{
+		private const string DOT = ".";
+		private const string GET_POOL_METHOD_NAME = nameof(World.Pool);
+		private const string ALLOCATE_METHOD_NAME = nameof(IComponentPool.Allocate);
+		private const string GET_POOL_METHOD_FORMAT = "world." + GET_POOL_METHOD_NAME + "<{0}>()";
+		private const string ALLOCATE_METHOD_FORMAT = GET_POOL_METHOD_FORMAT + DOT + ALLOCATE_METHOD_NAME + "(entity)";
+
+		public static string GenerateDefinition(int componentFieldsCount) => componentFieldsCount switch
+		{
+			0 => string.Empty,
+			_ => @$"
+		private readonly {nameof(IAuthoring)} _authoring;
+
+		public Baker({nameof(IAuthoring)} authoring) => _authoring = authoring;
+"
+		};
+
+		public static string GenerateAllocation(string componentName, string authoringName,
+			IReadOnlyList<FieldInfo> componentFields) => componentFields.Count switch
+		{
+			0 => string.Format(ALLOCATE_METHOD_FORMAT, componentName) + ";",
+			1 => string.Format(ALLOCATE_METHOD_FORMAT, componentName) +
+			     $".{componentFields[0].Name} = (({authoringName})_authoring).{componentFields[0].Name.ToCamelCaseWithUnderscore()};",
+			_ => $"ref var component = ref {string.Format(ALLOCATE_METHOD_FORMAT, componentName)};\n\t\t\t\t" +
+			     string.Join("\n\t\t\t\t", componentFields.Select(field =>
+				     $"component.{field.Name} = (({authoringName})_authoring).{field.Name.ToCamelCaseWithUnderscore()};"))
+		};
+	}
+}
